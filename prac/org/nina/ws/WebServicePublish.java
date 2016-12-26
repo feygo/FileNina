3c53487733c928412613134d7df0a71a52eb9e38
feygo <jonasjocstar@gmail.com>
2016-12-26 23:50:09 +0800

    t2
    完成本地webservice发布/调用 实验
    建立note.txt
    建立prac目录，用于实验

--- prac/org/nina/ws/WebServicePublish.java
+++ prac/org/nina/ws/WebServicePublish.java
@@ -10,7 +10,7 @@ import javax.xml.ws.Endpoint;
 public class WebServicePublish {
 
     public static void main(String[] args) {
-        //定义WebService的发布地址，这个地址就是提供给外界访问Webervice的URL地址，URL地址格式为：http://ip:端口号/xxxx
+        //定义WebService的发布地址，这个地址提供给外界访问Webervice的URL地址，URL地址格式为：http://ip:端口号/xxxx
         //String address = "http://192.168.1.100:8989/";这个WebService发布地址的写法是合法的
         //String address = "http://192.168.1.100:8989/Webservice";这个WebService发布地址的是合法的
         String address = "http://127.0.0.1:9090/WS_Server1/Webservice1";
